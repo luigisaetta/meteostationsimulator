d702f7e8219a6dfc8f6fc400f3e3615711a59967
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2018-09-11 08:55:28 +0200

    modified data in JSON as int, float

--- meteosym.py
+++ meteosym.py
@@ -1,8 +1,13 @@
+#
 # Simulate a Meteo station
+# reads data from a file, select an interval (one day for example)
+# and send data as JSON msgs to an MQTT broker
+#
+# can change the speed using sleepTime
+#
 
 import csv 
 import json
-import os
 import sys
 from datetime import datetime
 import time
@@ -16,13 +21,18 @@ HOST = "wiotubuntu-workshopiot2018-ezxpt5yn.srv.ravcloud.com"
 
 clientID = "meteo1"
 TIMEOUT = 10
-TOPIC_NAME = 'device/meteo1/data'
+TOPIC_NAME = "device/meteo1/data"
+
+# enables MQTT logging
 DO_LOG = False
 
-# config for interval chosen
+# config for interval chosen (idmi, idmax)
 ID1 = 1531
-ID2 = 1656
+ID2 = 1626
 
+#
+# handles MQTT logging
+#
 def on_log(client, userdata, level, buf):
     if DO_LOG == True:
         print("log: ",buf)
@@ -32,7 +42,7 @@ def on_log(client, userdata, level, buf):
 #
 
 #
-# reading file name from command line args
+# reading data file name from command line args
 #
 fName = sys.argv[1]
 
@@ -41,7 +51,10 @@ print ("***********************")
 print ("Starting simulation....")
 print ("")
 print ("File name: ", fName)
+print ("")
+print ("")
 
+# connect to MQTT broker
 mqttClient = mqtt.Client(clientID, protocol=mqtt.MQTTv311)
 mqttClient.on_log = on_log
 
@@ -54,38 +67,45 @@ try:
         lin_num = 1
 
         for line in reader:
-            # line e' una List
-            # ignora la prima linea (header)
-            if lin_num > 1:
-                # print(line)
+            
+            # line is a List
+            # ignore first line (header)
+            if (lin_num > 1):
+
                 id = line[0]
-                ts = line[1]
-                temp = line[2]
-                hum = line[3]
-                light = line[4]
-                airq = line[5]
-                pm25 = line[6]
-                pm10 = line[7]
 
-                # build the JSON msg
-                msg = {}
-                msg['id'] = id
-                msg['ts'] = ts
-                msg['temp'] = temp
-                msg['hum'] = hum
-                msg['light'] = light
-                msg['airq'] = airq
-                msg['pm25'] = pm25
-                msg['pm10'] = pm10
-
-                msgJson = json.dumps(msg)
-                
                 if (int(id) >= ID1 and int(id) <= ID2):
-                    print ('Sending: ', datetime.utcfromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S'), msgJson)
+                    # print(line)
 
+                    # Unix timestamp of the read from sensors
+                    ts = line[1]
+                    temp = line[2]
+                    hum = line[3]
+                    light = line[4]
+                    airq = line[5]
+                    pm25 = line[6]
+                    pm10 = line[7]
+
+                    # build the JSON msg
+                    msg = {}
+                    msg['id'] = int(id)
+                    msg['ts'] = int(ts)
+                    msg['temp'] = float(temp)
+                    msg['hum'] = float(hum)
+                    msg['light'] = float(light)
+                    msg['airq'] = float(airq)
+                    msg['pm25'] = float(pm25)
+                    msg['pm10'] = float(pm10)
+
+                    msgJson = json.dumps(msg)
+                    
+                    print ('Sending: ', datetime.utcfromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S'), msgJson)
+                    
+                    # send the msg to the MQTT broker
                     (result, mid) = mqttClient.publish(TOPIC_NAME, msgJson)
 
                     if result != 0:
+                        # some problems ? try enabling logging
                         print (result, mid)
 
                     time.sleep(sleepTime)
@@ -93,8 +113,9 @@ try:
             lin_num = lin_num + 1
 
 except Exception:
-    print("Errore: file not found: ", fName)
-    print("Interrupted...")
+    print()
+    print('\n')
+    print('*** Error info: ', sys.exc_info()[0], sys.exc_info()[1])
     sys.exit(-1)
 
 
